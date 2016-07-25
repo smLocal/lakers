b2011c44f38951161bfb2d82d51c19256abe1bab
david szyszkowski <try2murkme@gmail.com>
2016-07-25 11:45:43 -0700

    added ss folder and testing link on readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 #A Post Game Page for the Fans
 [GitHub](https://smlocal.github.io/lakers/)
+
+##Screen shots
+
+
+##SS on Galaxy s5
+![s6](lakers/ss/ss galaxys5.png)
---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log("js loaded");
+// $(function(){
+//     var contentHeight = $('.content').height();
+//     var overflowHeight = $('.content > div').height();
+
+//     if (overflowHeight > contentHeight) {
+//         $('.footer').css({
+//             'position': 'relative',
+//             'bottom': 0
+//         });
+//     } else {
+//         $('.footer').css({
+//             'position': 'absolute',
+//             'bottom': '10px'
+//         });
+//     }
+// });
